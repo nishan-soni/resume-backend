8bdbbcd59ffe7e72d6eb937e36020f5b6c5c16ec
nishan-soni <nishan.soni2@gmail.com>
2021-07-01 18:52:50 -0600

    hi

--- index.js
+++ index.js
@@ -52,11 +52,3 @@ app.post('/create/:template', (req, res) => {
 app.get('/download-resume', (req, res) => {
     res.download(path.join(__dirname,'/resume-temp.pdf'), 'resume.pdf')
 })
-
-app.get('/get-template', (req,res) => {
-    res.download(__dirname+ "/template1/template1.html")
-})
-
-app.get('/send-file', (req,res) => {
-    res.sendFile(path.join(__dirname,'/resume-temp.pdf'), 'resume.pdf')
-})
